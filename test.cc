947e643072a0195065f89415d0dcf74fd8260fe8
Lucas Yong <lucasyong95@gmail.com>
2018-10-31 19:14:47 -0700

    Add files via upload

--- test.cc
+++ test.cc
@@ -1,22 +1,52 @@
-// test to delete a key
+/*
+CSCI389 Homework 3
+By Laura Yoshida & Lucas Yong
+
+Description
+Unit Tests for a Cache Implementation in C++
+
+This series of unit tests is grouped according to the four methods in the Cache API, namely get, set, del, and space_used.
+Each group will test a specific case of its method. The test passes if the sole logical assert in the test function passes, and vice-versa.
+
+Some helper functions are written in the 'helper' section so that we don't need to keep rewriting certain lines of code across different unit tests.
+
+We are testing:
+1) Kathy & Rhody
+2) Ezra & Joe
+3) Henry & Noah
+4) Our own code
+*/
 #include "cache.hh"
 #include <functional>
 #include <memory>
-#include <string>
+#include <cstring>
 #include <assert.h>
 
-std::string key = "foo";
-Cache::val_type val = "bar";
-Cache::val_type val2 = "car";
-Cache::index_type size = 3;
+using namespace std;
 
-// Our Hash Function, implemented as a functor by overloading the operator ().
-// The algorithm is:
-// - Split the string key into an array of characters
-// - Iterate over the array and take the product of the prime numbers that correspond to each ASCII char
-//    (e.g. 'C' is 67th on the ASCII table, and the 67th prime is 317)
-// Once we have the product, return it as the bucket number to store value associated with the key
-// Since any given integer is a product of primes, the only K-V pairs sharing the same bucket would be those whose keys are permutations of each other
+// Global Variables
+/* Keys */
+string key = "foo";
+string key2 = "goo";
+
+/* uint32_t Values */
+uint32_t numVal = 57;
+uint32_t numVal2 = 85;
+
+/* string Values */
+string strVal = "bar";
+string strVal2 = "car";
+
+// Default Hash Function 
+/*
+Implemented as a functor by overloading the operator ().
+The algorithm is:
+- Split the string key into an array of characters
+- Iterate over the array and take the product of the prime numbers that correspond to each ASCII char
+   (e.g. 'C' is 67th on the ASCII table, and the 67th prime is 317)
+Once we have the product, return it as the bucket number to store value associated with the key
+Since any given integer is a product of primes, the only K-V pairs sharing the same bucket would be those whose keys are permutations of each other
+*/
 struct MyHashFunc {
     // An array of the first 255 primes
     const uint32_t PRIME_NUMBERS[255] = {  2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
@@ -47,54 +77,173 @@ struct MyHashFunc {
     }
 };
 
-void test_delete() {
-    Cache c(10);
-    c.set(key, val, size);
-    c.del(key);
-    Cache::val_type val = c.get(key,size);
-    assert(val==nullptr);
+// == HELPER FUNCTIONS ==
+Cache::val_type getBlobFromNum(uint32_t *num) {
+    Cache::val_type blobFromNum = reinterpret_cast<Cache::val_type>(num);
+    return blobFromNum;
+}
+Cache::val_type getBlobFromString(string *str) {
+    Cache::val_type blobFromString = reinterpret_cast<Cache::val_type>(str);
+    return blobFromString;
+}
+uint32_t getNumFromBlob(Cache::val_type blob) {
+    const uint32_t *numFromBlob = reinterpret_cast<const uint32_t*>(blob);
+    return *numFromBlob;
+}
+string getStringFromBlob(Cache::val_type blob) {
+    const string *stringFromBlob = reinterpret_cast<const string*>(blob);
+    return *stringFromBlob;
 }
 
-void  test_insert() {
-    Cache c(10);
-    c.set(key, val, size);
-    Cache::val_type get_val = c.get(key, size);
-    assert(val==get_val);
+/* == Cache::get  Tests== */
+// Check that we can insert a string value and get back the same value and size.
+void test_insert_string() {
+    Cache c(100, [](){ return 0; }, MyHashFunc());
+    Cache::val_type blob = getBlobFromString(&strVal);
+    c.set(key, blob, sizeof(strVal));
+    Cache::index_type get_size;
+    string get_val = getStringFromBlob(c.get(key, get_size));
+    assert(strVal==get_val);
+    assert(sizeof(strVal)==get_size);
 }
 
-void test_del_nonexistent() {
-    Cache c(10);
-    c.del(key);
-    // what should happen here
-    //assert(val==nullptr);
+// Check that we can insert an integer value and get back the same value and size.
+// *** changed to num instead of char ***
+void test_insert_num() {
+    Cache c(100, [](){ return 0; }, MyHashFunc());
+    Cache::val_type blob = getBlobFromNum(&numVal);
+    c.set(key, blob, sizeof(numVal));
+    Cache::index_type get_size;
+    uint32_t get_val = getNumFromBlob(c.get(key, get_size));
+    assert(numVal==get_val);
+    assert(sizeof(numVal)==get_size);
 }
 
-void  test_get_nonexistent() {
-    Cache c(10);
-    Cache::val_type get_val = c.get(key, size);
+// Try to get a nonexistent value from the Cache. get() should return nullptr.
+void test_get_nonexistent() {
+    Cache c(100, [](){ return 0; }, MyHashFunc());
+    Cache::index_type get_size;
+    Cache::val_type get_val = c.get(key, get_size);
     assert(get_val==nullptr);
 }
 
-
+// Check that when you set a K-V pair with an existing key (one that's already in the Cache), you get back the latest value that was set.
 void test_get_modified() {
-    Cache c(10);
-    c.set(key, val, size);
-    c.set(key, val2, size);
-    Cache::val_type get_val = c.get(key, size);
-    assert(get_val==val2);
+    Cache c(100, [](){ return 0; }, MyHashFunc());
+    Cache::val_type blob1 = getBlobFromNum(&numVal);
+    Cache::val_type blob2 = getBlobFromNum(&numVal2);
+    c.set(key, blob1, sizeof(numVal));
+    c.set(key, blob2, sizeof(numVal2));
+    Cache::index_type get_size;
+    uint32_t get_val = getNumFromBlob(c.get(key, get_size));
+    assert(get_val==numVal2);
+    assert(get_size==sizeof(numVal2));
 }
 
-/*
-void test_get_evicted() {
-    
+// Check that you can get some value after setting it and another one after it.
+void test_get_after_set() {
+    Cache c(100, [](){ return 0; }, MyHashFunc());
+    Cache::val_type blob1 = getBlobFromNum(&numVal);
+    Cache::val_type blob2 = getBlobFromNum(&numVal2);
+    c.set(key, blob1, sizeof(numVal));
+    c.set(key2, blob2, sizeof(numVal2));
+    Cache::index_type get_size;
+    uint32_t get_val = getNumFromBlob(c.get(key, get_size));
+    assert(get_val==numVal);
+    assert(get_size==sizeof(numVal));
 }
-*/
+
+// Check that you cannot get a value that has already been evicted.
+void test_get_evicted() {
+    Cache c(5, [](){ return 0; }, MyHashFunc());
+    Cache::val_type blob1 = getBlobFromNum(&numVal);
+    Cache::val_type blob2 = getBlobFromNum(&numVal2);
+    c.set(key, blob1, sizeof(numVal));
+    c.set(key2, blob2, sizeof(numVal2));
+    Cache::index_type get_size;
+    Cache::val_type get_val = c.get(key, get_size);
+    assert(get_val == nullptr);
+}
+
+// Check that you can get a value that triggered an eviction.
+void test_get_after_eviction() {
+    Cache c(5, [](){ return 0; }, MyHashFunc());
+    Cache::val_type blob1 = getBlobFromNum(&numVal);
+    Cache::val_type blob2 = getBlobFromNum(&numVal2);
+    c.set(key, blob1, sizeof(numVal));
+    c.set(key2, blob2, sizeof(numVal2));
+    Cache::index_type get_size;
+    uint32_t get_val = getNumFromBlob(c.get(key2, get_size));
+    assert(get_val == numVal2);
+}
+
+/* == Cache::set  Tests== */
+// Check that you can't set a value that is larger than the Cache's maxmem.
+void test_too_big() {
+    Cache c(1, [](){ return 0; }, MyHashFunc());
+    Cache::val_type blob = getBlobFromNum(&numVal);
+    c.set(key, blob, sizeof(numVal));
+    Cache::index_type get_size;
+    Cache::val_type get_val = c.get(key, get_size);
+    assert(get_val==nullptr);
+}
+
+/* == Cache::del  Tests== */
+// Check that you can't get a value that you have deleted.
+void test_delete() {
+    Cache c(100, [](){ return 0; }, MyHashFunc());
+    Cache::val_type blob1 = getBlobFromNum(&numVal);
+    c.set(key, blob1, sizeof(numVal));
+    c.del(key);
+    Cache::index_type get_size;
+    Cache::val_type get_val = c.get(key,get_size);
+    assert(get_val==nullptr);
+}
+
+// Check that deleting a nonexistent key does nothing.
+void test_del_nonexistent() {
+    Cache c(100, [](){ return 0; }, MyHashFunc());
+    c.del(key);
+}
+
+// Check that deleting an evicted key does nothing.
+void test_del_evicted() {
+    Cache c(5, [](){ return 0; }, MyHashFunc());
+    Cache::val_type blob1 = getBlobFromNum(&numVal);
+    Cache::val_type blob2 = getBlobFromNum(&numVal2);
+    c.set(key, blob1, sizeof(numVal));
+    c.set(key2, blob2, sizeof(numVal2));
+    c.del(key);
+}
+
+/* == Cache::space_used Tests== */
+// Check that the space used as reported by the Cache is consistent with the actual space used.
+void check_space_used() {
+    Cache c(100, [](){ return 0; }, MyHashFunc());
+    Cache::val_type blob1 = getBlobFromNum(&numVal);
+    Cache::val_type blob2 = getBlobFromNum(&numVal2);
+    Cache::index_type totalSpace = 0;
+    assert(c.space_used() == totalSpace);
+    c.set(key, blob1, sizeof(numVal));
+    totalSpace += sizeof(numVal);
+    assert(c.space_used() == totalSpace);
+    c.set(key2, blob2, sizeof(numVal2));
+    totalSpace += sizeof(numVal2);
+    assert(c.space_used() == totalSpace);
+}
 
 int main() {
-    test_delete();
-    test_insert();
-    test_del_nonexistent();
-    test_get_nonexistent();
-    test_get_modified();
+    test_insert_string(); // FAIL
+    test_insert_num(); // FAIL
+    test_get_nonexistent(); // PASS
+    test_get_modified(); // FAIL
+    test_get_after_set(); // FAIL
+    test_get_evicted(); // PASS
+    test_get_after_eviction(); // PASS
+    test_too_big(); // FAIL
+    test_delete(); // PASS
+    test_del_nonexistent() // PASS
+    test_del_evicted(); //PASS
+    check_space_used(); // PASS
     return 0;
-}
+} 
